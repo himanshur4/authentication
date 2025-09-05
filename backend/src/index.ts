38b5e9fc5a28d136634a2b1a29eff0731185eea5
Himanshu Raj <hraj808404@gmail.com>
2025-09-05 23:20:14 +0530

    feat: implement authentication routes, error handling, and user model with password hashing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,8 @@ import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env";
 import cors from "cors"
 import cookieParser from "cookie-parser";
 import errorHandler from "./middleware/errorHandler";
+import catchErrors from "./utils/catchErrors";
+import authRoutes from "./routes/auth.route";
 
 const app=express();
 app.use(express.json());
@@ -14,19 +16,17 @@ app.use(cors({
 }))
 app.use(cookieParser());
 
-app.get("/",async(req,res,next)=>{
-    try {
-        throw new Error("This is test error");
-        res.status(200).json({
+app.get("/",catchErrors(async(req,res,next)=>{
+    return res.status(200).json({
         status:"healthy",
     })
-    } catch (error) {
-        next(error)
-    }
-});
+}));
+
+app.use("/auth", authRoutes);
+
+
 
 app.use(errorHandler);
-
 app.listen(PORT, async()=>{
     console.log(`Server is running on port ${PORT} in ${NODE_ENV} environment.`);
     await connectToDatabase()
--- backend/src/utils/catchErrors.ts
+++ backend/src/utils/catchErrors.ts
@@ -1,19 +1,20 @@
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response } from "express"
 
-type AsyncController = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => Promise<any>;
+type AsyncController=(
+  req:Request,
+  res:Response,
+  next:NextFunction
+)=>Promise<any>;
 
-const catchErrors =
-  (controller: AsyncController): AsyncController =>
-  async (req, res, next) => {
+
+const catchErrors=(controller:AsyncController):AsyncController=>
+  async(req,res,next)=>{
     try {
-      await controller(req, res, next);
+      await controller(req,res,next);
     } catch (error) {
       next(error);
     }
-  };
+  }
 
-export default catchErrors;
+
+export default catchErrors
